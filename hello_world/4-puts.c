a79ba87e273cc1ab459b8930431035276a3775fe
theTropicalBoi <9804@holbertonstudents.com>
2024-10-21 12:27:35 +0200

    Created the infamous print hello world in C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,7 @@
 * Description: Prints a message to stdout using puts.
 * Return: Always 0 if Success.
 */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
