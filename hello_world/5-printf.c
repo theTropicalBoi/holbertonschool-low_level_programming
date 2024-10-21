338aa771a8ab589497f1a3ffa0759428d2384b86
theTropicalBoi <9804@holbertonstudents.com>
2024-10-21 12:37:15 +0200

    Task 5: Prints a message to stdout using printf.

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Prints a message to stdout using printf.
+ * Return: Always 0 if Success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
