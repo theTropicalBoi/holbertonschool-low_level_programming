02a96a54f07d7040dd824ce2c4d876f7790fc9e9
theTropicalBoi <9804@holbertonstudents.com>
2024-10-22 15:43:19 +0200

    Task 9: Tried it without going in depth

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * 
+ */
+int main(void)
+{
+    int i = 0;
+
+    while (i <= 9) {
+        putchar(i + '0');
+        putchar(',');
+        putchar(' ');
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
