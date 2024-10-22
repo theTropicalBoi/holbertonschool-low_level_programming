5341d4782152bfb2c726d90b736be853eef92e40
theTropicalBoi <9804@holbertonstudents.com>
2024-10-22 12:45:34 +0200

    Task 5: Print all single digit of base 10 from 0 using a for loops and the printf function.

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Print all single digit of base 10 from 0.
+ * Return: Always 0 if succeed.
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+printf("%d", n);
+printf("\n");
+
+return (0);
+}
