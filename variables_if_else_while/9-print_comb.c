63c7c80bae2abd9bb7cde83f93d76dc8a2901d9c
theTropicalBoi <9804@holbertonstudents.com>
2024-10-22 15:45:24 +0200

    Task 9: Tried it without going in depth

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
- * 
+ * main - Entry Point
+ *
+ * Description: Prints all possible combinations of single-digit numbers.
+ * Return: Always 0 if succeed.
  */
 int main(void)
 {
