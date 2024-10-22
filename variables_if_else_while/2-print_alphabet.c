e25b636f7118a4a892b14acc0c15b24ccb086622
theTropicalBoi <9804@holbertonstudents.com>
2024-10-22 11:10:57 +0200

    Task 2: Used a for loop to print the alphabet in lower case using putchar.

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - EntrY Point
+ *
+ * Description: Printing the alphabet in lowercase.
+ * Return: Always 0 if succeed.
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter ++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
