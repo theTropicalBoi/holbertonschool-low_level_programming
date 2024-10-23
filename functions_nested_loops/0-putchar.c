2508989cdbc78b04d7273908e4a7d6afcc36782d
theTropicalBoi <9804@holbertonstudents.com>
2024-10-23 13:36:42 +0200

    FINISHED THE TASK 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * Description: Print "_putchar" header.
+ * Return: 0 if Succeed.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
