985e8683cac70f24edf55b32cdb73ceacf17e129
theTropicalBoi <9804@holbertonstudents.com>
2024-10-23 14:36:12 +0200

    Task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * 
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	
+}
