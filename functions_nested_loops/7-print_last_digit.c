29d952dc37f158f2ce86475449644d464fdca606
theTropicalBoi <9804@holbertonstudents.com>
2024-10-25 09:37:02 +0200

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit of a number.
+ * @r: The number given.
+ * Return: The last digit stored in the variable i.
+ */
+int print_last_digit(int r)
+{
+	int i = r % 10;
+
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+	_putchar('0' + i);
+	return (i);
+}
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day from 00:00 to 23:59.
+ * Return: prints every minute of the day
+ */
+void jack_bauer(void)
