432f9e6398a467bf590c5fb7df0cc880f48f0474
theTropicalBoi <9804@holbertonstudents.com>
2024-10-28 11:08:04 +0100

    Task 1: if, else loop to check if c is digit. Added the main.h and the main.c for task 0 and 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - Function that check for digit from 0 to 9.
+ * @c: The variable that store the character to check.
+ * Return: 1 if c is digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 1 && c <= 9)
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+}
