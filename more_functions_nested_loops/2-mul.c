dd9c2c095801906fe023987722485ce87d65aac0
theTropicalBoi <9804@holbertonstudents.com>
2024-10-28 11:50:36 +0100

    Task 2: Function to multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multiplies 2 integer.
+ * @a: First digit to multiply
+ * @b: Second digit to multiply.
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
