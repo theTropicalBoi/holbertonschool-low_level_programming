69c99a700d0ae19930ba0835c788ee42b307a113
theTropicalBoi <9804@holbertonstudents.com>
2024-10-28 15:25:52 +0100

    Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - Function to print 10 times from 0 to 14.
+ * @numbers: Array that store the number.
+ * @list: counts the number of line.
+ */
+void more_numbers(void)
+{
+	int list;
+	int numbers;
+
+	for (list = 0; list < 10; list++)
+	{
+		for (numbers = 0; numbers <= 14; numbers++)
+		{
+			if (numbers >= 10)
+			{
+				_putchar((numbers / 10) + '0');
+			}
+			_putchar((numbers % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
