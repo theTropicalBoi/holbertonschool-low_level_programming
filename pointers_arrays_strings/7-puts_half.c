e5bd1cbe919c93c657da64e7890764ec07e3026f
theTropicalBoi <9804@holbertonstudents.com>
2024-11-03 16:55:35 +0100

    Task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/** 
+* puts_half - Function that swaps the values of two integers.
+
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i - 1 / 2;
+	i = 5;
+
+	while (i < j)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
