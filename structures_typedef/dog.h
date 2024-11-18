a1262c279e38d5d70012413962d4f8ad7e29add7
theTropicalBoi <9804@holbertonstudents.com>
2024-11-18 16:46:45 +0100

    Header file modified

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,8 +13,10 @@ typedef struct dog
 	char *owner;
 }
 
-typedef struct dog dog_t;
+/* typedef struct dog dog_t; */
 
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 /* Prototype: */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
