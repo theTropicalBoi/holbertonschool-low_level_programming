fcb568b910861ff8e498fabfe9e2ce611856df0e
theTropicalBoi <9804@holbertonstudents.com>
2024-12-05 13:00:44 +0100

    Clean the code for betty style check

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * sum_dlistint- Function that return the sum of a list.
- * Pointer to the head of the list.
+ * @head: Pointer to the head of the list.
  * Return: Sum of all data, 0 if list is empty.
  */
 int sum_dlistint(dlistint_t *head)
